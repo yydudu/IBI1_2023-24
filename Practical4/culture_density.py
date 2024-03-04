8b6a48976755232254d938b27ba68d7552ea2b7f
y09 <1556085378@qq.com>
2024-03-04 23:30:09 +0800

    initialise

---
+++ Practical4/culture_density.py
@@ -0,0 +1,7 @@
+density=5 #the initial density is 5%
+day=1
+while density<=90:
+    density =density*2  #the cell density doubles in density per day
+    day+=1
+print("I can stay away from lab for",day,"days")
+print("On day", day,"the cell density is larger than 90%")
