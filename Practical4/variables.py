3fb1fb6795b390022499ad75eecddc4cde1d136d
y09 <1556085378@qq.com>
2024-03-04 23:33:27 +0800

    initialise

---
+++ Practical4/variables.py
@@ -0,0 +1,23 @@
+a=40
+b=36
+c=30
+d=a-b 
+e=b-c 
+if d > e:
+    print("The improvement from running is greater.")
+    print("Running only training had a greater improvement on running time.")
+elif e > d:
+    print("The improvement from running and strength training is greater.")
+    print("Running and strength training had a greater improvement on running time.")
+else:
+    print("The improvements from running only and running and strength training are equal.")
+X=True
+Y=False
+W=X or Y 
+print(W)
+# Truth table
+# X      Y      W
+# True   True   True
+# True   False  True
+# False  True   True  
+# False   False False
