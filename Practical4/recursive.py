0226bd298cb7c9b453a9dbbaae1f30511cde10db
y09 <1556085378@qq.com>
2024-03-05 16:38:34 +0800

    iniialise

--- Practical4/recursive.py
+++ Practical4/recursive.py
@@ -1,10 +1,4 @@
-a1=4
-a2=11
-a3=25
-print(a1)
-print(a2)
-print(a3)
-for n in range(4,6):
-    an = 2*a3+3
-    print(an)
-    a3=an
+a = 4  #Assign an initial value
+for i in range(5):  #cycle 5 times
+    print(a)    #print the outcomes
+    a = 2*a +3    #update its value and cycle
