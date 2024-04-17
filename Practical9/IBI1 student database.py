dd73acf77dd763377b7b12d0bb721fd2b7687106
y09 <1556085378@qq.com>
2024-04-17 23:06:36 +0800

    initialise

--- Practical9/IBI1 student database.py
+++ Practical9/IBI1 student database.py
@@ -1,16 +1,16 @@
-class Students: #class
+class Student: #class
     #define a function
     def __init__(self,name,major,code_portfolio_score,group_score,exam):
         self.name=name
         self.major=major
-        self.code_portfolio_score=code_portfolio_score
-        self.group_score=group_score
-        self.exam=exam
+        self.code_portfolio_score=max(0,min(code_portfolio_score,100))
+        self.group_score=max(0,min(group_score,100))
+        self.exam=max(0,min(exam,100))
     
     def print_details(self):
         print(f'{self.name}, Major: {self.major}, Code Portfolio: {self.code_portfolio_score}/100, '
               f'Group Project: {self.group_score}/100, Exam: {self.exam}/100')
 
 #example
-student=Students('Anna','BMI', 55, 65, 63)
+student=Student('Anna','BMI', 55, 65, 63)
 student.print_details()
